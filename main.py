12b0dfff898e03e64013ed30148e2887baea500b
Rohit Chaudhary <40576340+rorochaudhary@users.noreply.github.com>
2020-12-18 13:50:22 -0800

    implement retrieve canvas courses and select course

--- main.py
+++ main.py
@@ -17,13 +17,13 @@ layout = [
 # Create the Window
 window = sg.Window('Just Get Me By', layout)
 
-# Event Loop to process "events"
+# Main Event Loop to process "events"
 while True:             
     event, values = window.read()
     if event in (sg.WIN_CLOSED, 'Cancel'):
         break
 
-    # user clicks Help - display instructions to get token
+    # -------------Help Get Token Window (within mainloop)-----------
     if event in ("How to get a Token"):
         get_token_txt = "In order to calculate your grades, I will need a token:\n" + \
                         "1. Login to your institution Canvas site.\n" + \
@@ -35,47 +35,81 @@ while True:
                         "Screenshots of token generation are available at the GitHub repo - https://github.com/rorochaudhary/just-get-me-by"
         sg.popup_scrolled(get_token_txt, title="Getting a User Token")
     
-    # open window for grade calculator
+    # -------Select Course Window (within mainloop)-------------
     if event in ('Ok'):
         req_items = values
+        req_items['canvasURL'] = 'https://' + req_items['canvasURL'] # right place to format protocol??
         print("req_items:", req_items)
-        # calls to /lib/api/api.py here or package req_items to be sent
-        results = 'API RESULTS PASSED IN?'
-
-        # layout based on results
-        calc_layout = [
-            [sg.Text('Your Canvas course:')]
-        ]
         
-        # display assignments
-        for i in range(0, 6):
-            calc_layout += [sg.Text(f'assignment {i} here'), sg.Text(f'score {i} here'), sg.Text(f'total {i} here')],
+        # get Canvas information
+        requestAPI = Canvas(req_items['canvasURL'], req_items['token'])
+        courses = requestAPI.get_courses()
+        # print(courses)
 
-        # display grade weights
-        calc_layout += [sg.Text('canvas grade breakdown:')],
-        for i in range(0, 6):
-            calc_layout += [sg.Text(f'assignment group {i}'), sg.Text(f'group weight {i}'), sg.Text('total here')],
+        # just need course id and name
+        course_names = []
+        for i in range(len(courses)):
+            # print(f"Course {i}: {courses[i]}\n")
+            # print(f'id: {courses[i]["id"]}, name: {courses[i]["name"]}')
+            course_names.append({'id': courses[i]["id"], 'name': courses[i]["name"]})
+        print(f"current courses:\n{course_names}")
+        # print([course_names[i]['name'] for i in range(len(course_names))])
 
-        # display grading standard
-        calc_layout += [sg.Text('course grading standard:')],
-        for i in range(0, 6):
-            calc_layout += [sg.Text(f'grade title {i}'), sg.Text(f'grading scheme {i}')],
+        course_layout = [
+            [sg.Listbox(values=[course_names[i]['name'] for i in range(len(course_names))], size=(75, 12), key='selected_course')],
+            [sg.Button('Select'), sg.Button('Cancel')]
+        ]
 
-        # select target grade and execute grade calc
-        calc_layout += [sg.Text('Target Grade?'), sg.Combo(["A", "B", "C", "D", "E", "F"], size=(3, 1))],
-        calc_layout += [sg.Button('Just Get Me By'), sg.Cancel()],
-
-        # display window
-        calc_window = sg.Window('Grade Calculator', calc_layout, finalize=True)
+        course_window = sg.Window('Select Course', course_layout, finalize=True)
 
         while True:
-            calc_event, calc_values = calc_window.read()
-            if calc_event in (sg.WIN_CLOSED, 'Cancel'):
+            course_event, course_values = course_window.read()
+            if course_event in ('Cancel'):
                 break
-            if calc_event in ('Just Get Me By'):
-                # pop up showing calculator results? or manipulate/add to current window?
-                break
+            if course_event in ('Select'):
+                # print('user chose:', course_values['selected_course'][0])
+                # get selected course id
+                id = 0
+                for i in range(len(course_names)):
+                    if course_names[i]['name'] == course_values['selected_course'][0]:
+                        id = course_names[i]['id']
+                # print("selected:", id)
 
-        calc_window.close()
+                # ---------Grade Calculator Window (within Select Course loop)------------
+                # layout based on results
+                calc_layout = [
+                    [sg.Text('Your Canvas course:')]
+                ]
+                
+                # display assignments
+                for i in range(0, 6):
+                    calc_layout += [sg.Text(f'assignment {i} here'), sg.Text(f'score {i} here'), sg.Text(f'total {i} here')],
 
+                # display grade weights
+                calc_layout += [sg.Text('canvas grade breakdown:')],
+                for i in range(0, 6):
+                    calc_layout += [sg.Text(f'assignment group {i}'), sg.Text(f'group weight {i}'), sg.Text('total here')],
+
+                # display grading standard
+                calc_layout += [sg.Text('course grading standard:')],
+                for i in range(0, 6):
+                    calc_layout += [sg.Text(f'grade title {i}'), sg.Text(f'grading scheme {i}')],
+
+                # select target grade and execute grade calc
+                calc_layout += [sg.Text('Target Grade?'), sg.Combo(["A", "B", "C", "D", "E", "F"], size=(3, 1))],
+                calc_layout += [sg.Button('Just Get Me By'), sg.Cancel()],
+
+                # display window
+                calc_window = sg.Window('Grade Calculator', calc_layout, finalize=True)
+
+                while True:
+                    calc_event, calc_values = calc_window.read()
+                    if calc_event in (sg.WIN_CLOSED, 'Cancel'):
+                        break
+                    if calc_event in ('Just Get Me By'):
+                        # pop up showing calculator results? or manipulate/add to current window?
+                        break
+
+                calc_window.close()
+        course_window.close()
 window.close()
