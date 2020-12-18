87222099bc2cc864164c0cf4c1ef0d9260198523
mchen1235 <michaelschen123@gmail.com>
2020-12-18 15:47:38 -0800

    Added function that uses an api to get data and an algorithm to calculate grades

--- test.py
+++ test.py
@@ -70,23 +70,28 @@ COURSE_ID = 1784009
 # print('')
 
 
-#Data structure to pass into algo
-target = 93
-assignment_dict = {}
-group_dict = {}
-assignment_data = test.get_assignments_in_course(COURSE_ID)
-group_data = test.get_assignment_groups_in_course(COURSE_ID)
+def calculate_grades(target_grade, course_id) -> dict:
+    assignment_dict = {}
+    group_dict = {}
+    assignment_data = test.get_assignments_in_course(course_id)
+    group_data = test.get_assignment_groups_in_course(course_id)
 
-for group in group_data:
-    assignment_list = []
-    assignment_list.append(group['group_weight'])
-    group_dict[group['id']] = assignment_list
+    #creates a dict holding group ids: 'group_id': [group_weight]
+    for group in group_data:
+        assignment_list = []
+        assignment_list.append(group['group_weight'])
+        group_dict[group['id']] = assignment_list
 
-for assignment in assignment_data:
-    data_list = []
-    data_list.append(assignment['submission']['score'])
-    data_list.append(assignment['points_possible'])
-    assignment_dict[assignment['name']] = data_list
+    #creates a dict holding assigenment_data: 'name': [score, max_score]
+    #appends assignment names to the dict holding group ids
+    for assignment in assignment_data:
+        data_list = []
+        data_list.append(assignment['submission']['score'])
+        data_list.append(assignment['points_possible'])
+        assignment_dict[assignment['name']] = data_list
 
-    #append assignment to group
-    group_dict[assignment['assignment_group_id']].append(assignment['name'])
+        #append assignment to group
+        group_dict[assignment['assignment_group_id']].append(assignment['name'])
+
+    #uses the grade algorithm
+    return algo(target_grade, assignment_data, group_data)
