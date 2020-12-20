761d0a9d36b45d45c788fbb635728f9b1e93ed54
Allen J H <allenjhuang92@gmail.com>
2020-12-20 11:23:41 -0600

    Separated get_grade_scale functionality.

--- main.py
+++ main.py
@@ -90,7 +90,8 @@ while True:
 
                 #add assignment information to layout
                 raw_grade_standard = requestAPI.get_grading_standard_in_course(course_id)
-                calc_layout += gui.display_known_info(raw_grade_standard, assignment_dict)
+                grading_scheme = gui.get_grading_scheme(raw_grade_standard)
+                calc_layout += gui.display_known_info(grading_scheme, assignment_dict)
 
                 # display window
                 calc_window = sg.Window('Grade Calculator', calc_layout, finalize=True)
